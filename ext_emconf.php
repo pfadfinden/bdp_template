67888d5743c3fd136d6999e736cb49ea049ff63c
Christian Schrebe <christian@schrebe.de>
2015-11-28 12:45:40 +0100

    Fehler für v1.1.0 beseitigt
    * Abhängigkeit gridelements: Version angepasst
    * Font: Falsche Schriftart im grid.css ersetzt
    * nofound.html: css angepasst, nun auch mobile

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'fluid' => '1.3 - 6.2.99',
 			'news' => '3.2.0 - 3.9.99',
 			'filemetadata' => '6.2.0 - 6.2.99',
-			'gridelements' => '3.2.0 - 3.2.99',
+			'gridelements' => '3.2.0 - 3.9.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
