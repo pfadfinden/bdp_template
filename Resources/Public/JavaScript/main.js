1a0a33c769c8f38f61cd2df00aced0bbc250688a
Christian Schrebe <christian@schrebe.de>
2016-02-19 18:12:33 +0100

    optimize lightbox

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -18,7 +18,24 @@ $(document).ready(function() {
     navSecondary();
   });
 
-  $(".lightbox").fancybox();
+  $(".lightbox")
+      .fancybox({
+        padding : 0,
+        afterShow: function() {
+          $(".fancybox-title").wrapInner('<div />').show();
+
+          $(".fancybox-wrap").hover(function() {
+            $(".fancybox-title").show();
+          }, function() {
+            $(".fancybox-title").hide();
+          });
+        },
+        helpers : {
+          title: {
+            type: 'over'
+          }
+        }
+      });
 });
 
 $(function() {
