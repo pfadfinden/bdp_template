74e69d0762b2cf020459dfd4551c67fcd0474f79
Christian Schrebe <christian@schrebe.de>
2021-04-03 12:43:13 +0200

    update headings

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -57,7 +57,7 @@ Encore
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+    //.enableVersioning(Encore.isProduction())
 
     // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
