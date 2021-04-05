c9b70b9f4b06d3d4474d3f56423accabd872b51c
Christian Schrebe <christian@schrebe.de>
2021-04-05 13:32:37 +0200

    new design for Footer.html

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -27,6 +27,18 @@ Encore
         Fonts: path.resolve( __dirname,'Resources/Private/Source/Fonts/')
     })
 
+    // Copy some static images to your -> https://symfony.com/doc/current/frontend/encore/copy-files.html
+    .copyFiles({
+        from: path.resolve( __dirname,'Resources/Private/Source/Images/'),
+        // Optional target path, relative to the output dir
+        //to: 'images/[path][name].[ext]',
+        includeSubdirectories: true,
+        // if versioning is enabled, add the file hash too
+        to: 'images/[path][name].[hash:8].[ext]',
+        // only copy files matching this pattern
+        pattern: /\.(png|jpg|jpeg|svg)$/
+    })
+
     /*
      * ENTRY CONFIG
      *
