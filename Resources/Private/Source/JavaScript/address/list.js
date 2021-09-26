599c84b3f43746756edf0f95a32ac285d61d9bd6
Christian Schrebe <christian@schrebe.de>
2021-09-26 09:44:07 +0200

    text with image and tt_address optimisation

--- Resources/Private/Source/JavaScript/address/list.js
+++ Resources/Private/Source/JavaScript/address/list.js
@@ -7,9 +7,12 @@
 import 'Scss/9-stand-alone/address-list.scss'
 import Masonry from "masonry-layout";
 
-const list = new Masonry( '.ce-address__list__items', {
-    itemSelector: '.ce-address__list__items__item',
-    columnWidth: '.ce-address__list__items__item',
-    percentPosition: true,
-    transitionDuration: '0.2'
-});
+const items = document.querySelectorAll('.ce-address__list__items')
+items.forEach(element => {
+    new Masonry(element, {
+        itemSelector: '.ce-address__list__items__item',
+        columnWidth: '.ce-address__list__items__item',
+        percentPosition: true,
+        transitionDuration: '0.2'
+    });
+})
