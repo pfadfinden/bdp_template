64908977cd50c9f7a2b90eb03219e60f90643711
Christian Schrebe <christian@schrebe.de>
2021-10-02 10:58:15 +0200

    [TASK] Prepare for 4.1.0

--- Resources/Private/Source/JavaScript/main.js
+++ Resources/Private/Source/JavaScript/main.js
@@ -1,5 +1,5 @@
 /*
- * BdP Template v4.0.0 (https://www.pfadfinden.de)
+ * BdP Template v4.1.0 (https://www.pfadfinden.de)
  * Copyright 2021 Christian Schrebe
  * Licensed under the GPL-2.0-or-later license
  */
