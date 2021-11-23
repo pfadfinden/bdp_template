e43c333d2df451f7c11fcbb3bf15ee9fdc6062ce
Christian Schrebe <christian@schrebe.de>
2021-11-23 17:53:51 +0100

    [FEATURE] Add `Call to Action Button´ to Page Header
    In the siteroot page a `Call to Action Button´ can be activated.
    This Button is shown only on large screens on all pages.
    
    Resolves: #90

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,3 +4,11 @@
 CREATE TABLE tt_content (
     image_position smallint unsigned DEFAULT '0' NOT NULL
 );
+
+#
+# Table structure for table 'pages'
+#
+CREATE TABLE pages (
+    bdp_button_value varchar(255) DEFAULT '' NOT NULL,
+    bdp_button_link varchar(1024) DEFAULT '' NOT NULL,
+);
