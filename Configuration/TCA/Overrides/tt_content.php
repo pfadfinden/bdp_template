b3c9772e5b565223e82581d5a132b0f0a50ed295
Christian Schrebe <christian@schrebe.de>
2026-08-26 10:43:08 +0200

    Layout for default content elements

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -35,19 +35,19 @@ defined('TYPO3') || die();
                     'name' => 'Inhalt',
                     'colPos' => 110,
                     'colspan' => 1,
-                    'allowedContentTypes'=> 'pfadfinden_texttopimage, pfadfinden_smallctaimage'
+                    'allowedContentTypes'=> 'pfadfinden_texttopimage, pfadfinden_smallctaimage, text'
                 ],
                 [
                     'name' => 'Inhalt',
                     'colPos' => 120,
                     'colspan' => 1,
-                    'allowedContentTypes'=> 'pfadfinden_texttopimage, pfadfinden_smallctaimage'
+                    'allowedContentTypes'=> 'pfadfinden_texttopimage, pfadfinden_smallctaimage, text'
                 ],
                 [
                     'name' => 'Inhalt',
                     'colPos' => 130,
                     'colspan' => 1,
-                    'allowedContentTypes'=> 'pfadfinden_texttopimage, pfadfinden_smallctaimage'
+                    'allowedContentTypes'=> 'pfadfinden_texttopimage, pfadfinden_smallctaimage, text'
                 ]
             ],
 
@@ -75,7 +75,7 @@ defined('TYPO3') || die();
                     'name' => 'Inhalt',
                     'colPos' => 120,
                     'colspan' => 1,
-                    'allowedContentTypes'=> 'pfadfinden_texttopimage'
+                    'allowedContentTypes'=> 'pfadfinden_texttopimage, pfadfinden_smallctaimage, text'
                 ]
             ],
 
@@ -111,76 +111,4 @@ defined('TYPO3') || die();
     )
         // override default configurations
         ->setSaveAndCloseInNewContentElementWizard(false)
-);
-
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
-    'tt_content',
-    [
-        'header_color' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:bdp_template/Resources/Private/Language/locallang_db.xlf:tt_content.header_color',
-            'config' => [
-                'type' => 'select',
-                'renderType' => 'selectSingle',
-                'items' => [
-                    ['label' => 'Default', 'value' => ''],
-                    ['label' => 'Black', 'value' => 'black'],
-                    ['label' => 'White', 'value' => 'white'],
-                ],
-                'default' => ''
-            ],
-        ],
-        'header_design' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:bdp_template/Resources/Private/Language/locallang_db.xlf:tt_content.header_design',
-            'config' => [
-                'type' => 'select',
-                'renderType' => 'selectSingle',
-                'items' => [
-                    [
-                        'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.default_value',
-                        'value' => '0'
-                    ],
-                    [
-                        'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_layout.I.1',
-                        'value' => '1'
-                    ],
-                    [
-                        'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_layout.I.2',
-                        'value' => '2'
-                    ],
-                    [
-                        'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_layout.I.3',
-                        'value' => '3'
-                    ],
-                    [
-                        'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_layout.I.4',
-                        'value' => '4'
-                    ],
-                    [
-                        'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_layout.I.5',
-                        'value' => '5'
-                    ],
-                    [
-                        'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_layout.I.6',
-                        'value' => '100'
-                    ]
-                ],
-                'default' => 0
-            ]
-        ],
-    ]
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
-    'tt_content',
-    'header',
-    'header_design,header_color',
-    'after:header_layout'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
-    'tt_content',
-    'headers',
-    'header_design,header_color',
-    'after:header_layout'
 );
